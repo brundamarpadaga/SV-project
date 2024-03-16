50864fd19c86feba947927b0a1f0b93d2f7c28d6
jonam987 <manojmur@pdx.edu>
2024-03-16 14:45:17 -0700

    Interfaced Module

---
+++ interface.sv
@@ -0,0 +1,40 @@
+interface  Intel8088Pins (input logic CLK,RESET);  
+
+	logic MNMX = '1;
+	logic TEST = '1;
+	logic READY = '1;
+	logic NMI = '0;
+	logic INTR = '0;
+	logic HOLD = '0;
+
+	logic HLDA;
+	tri [7:0] AD;
+	tri [19:8] A;
+	logic CS;
+	logic IOM;
+	logic WR;
+	logic RD;
+	logic SSO;
+	logic INTA;
+	logic ALE;
+	logic DTR;
+	logic DEN;
+	
+	logic [19:0] Address;
+	wire [7:0]  Data;
+
+    modport  Processor (input CLK, RESET, MNMX,TEST,READY,NMI,INTR,HOLD,
+                        output ALE,IOM,WR,RD,DTR,DEN,SSO,A,HLDA,INTA,	
+						inout AD
+                         );
+
+    modport Peripheral (
+						input CLK,
+						input RESET,
+						inout Data,
+						input Address,
+						input ALE,
+						input RD,
+						input WR);   
+
+endinterface
--- top-3.sv
+++ top-3.sv
@@ -1,69 +1,49 @@
 module top;
-parameter M = 0;
-bit CLK = '0;
-bit MNMX = '1;
-bit TEST = '1;
-bit RESET = '0;
-bit READY = '1;
-bit NMI = '0;
-bit INTR = '0;
-bit HOLD = '0;
+logic CLK = '0;
+logic RESET = '0;
 
-wire logic [7:0] AD;
-logic [19:8] A;
-logic HLDA;
-logic IOM;
-logic WR;
-logic RD;
-logic SSO;
-logic INTA;
-logic ALE;
-logic DTR;
-logic DEN;
 logic IO_CS0,IO_CS1;
 logic M_CS0,M_CS1;
-
-logic [19:0] Address;
-wire [7:0]  Data;
-
-Intel8088 P(CLK, MNMX, TEST, RESET, READY, NMI, INTR, HOLD, AD, A, HLDA, IOM, WR, RD, SSO, INTA, ALE, DTR, DEN);
+	
+Intel8088Pins i(CLK,RESET);
+Intel8088 P(.i(i.Processor)); 
 
 //Instantiate 4 Modules
-IOM #(.size(2**19)) Mem0 (CLK,RESET,Address,M_CS0,RD,WR,ALE,Data);
-IOM #(.size(2**19)) Mem1 (CLK,RESET,Address,M_CS1,RD,WR,ALE,Data);
-IOM #(.size(2**4)) IO_0 (CLK,RESET,Address,IO_CS0,RD,WR,ALE,Data);
-IOM #(.size(2**9)) IO_1 (CLK,RESET,Address,IO_CS1,RD,WR,ALE,Data);
+IOM #(.size(2**19)) Mem0 (.i(i.Peripheral),.CS(M_CS0));
+IOM #(.size(2**19)) Mem1 (.i(i.Peripheral),.CS(M_CS1));
+IOM #(.size(2**4)) IO_0 (.i(i.Peripheral),.CS(IO_CS0));
+IOM #(.size(2**9)) IO_1 (.i(i.Peripheral),.CS(IO_CS1));
 
 // 8282 Latch to latch bus address
 always_latch
 begin
-if (ALE)
-	Address <= {A, AD};
+if (i.ALE)
+	i.Address <= {i.A, i.AD};
 end
 
 // 8286 transceiver
-assign Data =  (DTR & ~DEN) ? AD   : 'z;
-assign AD   = (~DTR & ~DEN) ? Data : 'z;
+assign i.Data =  (i.DTR & ~i.DEN) ? i.AD   : 'z;
+assign i.AD   = (~i.DTR & ~i.DEN) ? i.Data : 'z;
 
 //Chipselect Logic
-assign M_CS0 = ~(~Address[19] & ~IOM);											//Active Low
-assign M_CS1 = ~(Address[19] & ~IOM);										//Active Low
-assign IO_CS0 = ~((Address[15:8] & ~Address[7:4]) & IOM);						//Active Low
-assign IO_CS1 = ~((~Address[15:13] & Address[12:10] & ~Address[9]) & IOM);	//Active Low
+assign M_CS0 = ~(~i.Address[19] & ~i.IOM);											//Active Low
+assign M_CS1 = ~(i.Address[19] & ~i.IOM);										//Active Low
+assign IO_CS0 = ~((i.Address[15:8] & ~i.Address[7:4]) & i.IOM);						//Active Low
+assign IO_CS1 = ~((~i.Address[15:13] & i.Address[12:10] & ~i.Address[9]) & i.IOM);	//Active Low
 
 always #50 CLK = ~CLK;
 
 initial
 begin
-$dumpfile("dump.vcd"); $dumpvars;
+	$dumpfile("dump.vcd"); $dumpvars;
 
-repeat (2) @(posedge CLK);
-RESET = '1;
-repeat (5) @(posedge CLK);
-RESET = '0;
+	repeat (2) @(posedge CLK);
+	RESET = '1;
+	repeat (5) @(posedge CLK);
+	RESET = '0;
 
-repeat(10000) @(posedge CLK);
-$finish();
+	repeat(10000) @(posedge CLK);
+	$finish();
 end
 
 endmodule
